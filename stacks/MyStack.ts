f36468e0878d6ee3acc7da4dd1f2b5c26aed5479
Frank <wangfanjie@gmail.com>
2023-12-12 11:59:07 -0500

    arm

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -4,6 +4,7 @@ export function API({ stack }: StackContext) {
   const fn = new Function(stack, "fn", {
     runtime: "python3.9",
     handler: "src/lambda-python/handler.handler",
+    architecture: "arm_64",
     url: true,
   });
 
